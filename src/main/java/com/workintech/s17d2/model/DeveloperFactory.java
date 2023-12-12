f04b8c46dd348773f101b473e6ba71a67200a16b
mrhkzltn <emrahkiziltan94@gmail.com>
2023-12-12 18:31:45 +0300

    s17d2 solution.

---
+++ src/main/java/com/workintech/s17d2/model/DeveloperFactory.java
@@ -0,0 +1,23 @@
+package com.workintech.s17d2.model;
+
+import com.workintech.s17d2.tax.Taxable;
+import com.workintech.s17d2.utils.Constants;
+
+public class DeveloperFactory {
+
+
+
+    public static Developer createDeveloper(Developer developer, Taxable taxable) {
+        Developer createdDeveloper = null;
+        if (developer.getExperience().name().equalsIgnoreCase(Constants.JUNIOR)) {
+            createdDeveloper = new JuniorDeveloper(developer.getId(), developer.getName(),
+                    (developer.getSalary() - (developer.getSalary() * taxable.getSimpleTaxRate()) / 100));
+        } else if (developer.getExperience().name().equalsIgnoreCase(Constants.MID)) {
+            createdDeveloper = new MidDeveloper(developer.getId(), developer.getName(), (developer.getSalary() - (developer.getSalary() * taxable.getMiddleTaxRate()) / 100));
+        } else if (developer.getExperience().name().equalsIgnoreCase(Constants.SENIOR)) {
+            createdDeveloper = new SeniorDeveloper(developer.getId(), developer.getName(), (developer.getSalary() - (developer.getSalary() * taxable.getUpperTaxRate()) / 100));
+        }
+
+        return createdDeveloper;
+    }
+}
